5e6c3bc3bfaf6972a5eb416a14619957b0f28aa9
Xiaken <jaybayron400@gmail.com>
2021-10-01 23:35:22 +0800

    commit

---
+++ dashboard.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css\Resetstylesheet.css">
+	<link rel="stylesheet" type="text/css" href="css\dashboard.css">
+	<meta charset="utf-8">
+	<title>Human Resources</title>
+</head>
+<body>
+	<h1>Human Resources</h1>
+	<a href="">Log Out</a>
+	<select name="department">
+		<option hidden="">Choose Department</option>
+		<option value="1">Department 1</option>
+		<option value="2">Department 2</option>
+		<option value="3">Department 3</option>
+	</select>
+	<form action="">
+	  	<input type="text">
+	  	<input type="submit" value="Search">
+	</form>
+	<input type="submit" value="Add Employee">
+	<h2>Employee's Details</h2>
+	<table>
+	  	<tr>
+		    <th>Id</th>
+		    <th>Surname</th>
+		    <th>First Name</th>
+		    <th>Middle Name</th>
+		    <th>Age</th>
+		    <th>Birth Date</th>
+		    <th>Gender</th>
+		    <th>City</th>
+		    <th>Address</th>
+		    <th>Postal Code</th>
+		    <th>Position</th>
+		    <th>Contract</th>
+		    <th>Start</th>
+		    <th>End</th>
+		    <th>Email</th>
+		    <th>Date Created</th>
+		    <th>Date Updated</th>
+		    <th>Remarks</th>
+		    <th>Action</th>
+	  	</tr>
+	  	<tr>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>
+			<td><?php /*php code*/ ?></td>			
+  		</tr>
+	</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css\Resetstylesheet.css">
+	<link rel="stylesheet" type="text/css" href="css\index.css">
+	<meta charset="utf-8">
+	<title>Human Resources</title>
+</head>
+<body>
+	<h1>Human <span>Resources</span></h1>
+	<a href="">About</a>
+	<form action="">
+	  	<input type="text" placeholder="Username">
+	  	<input type="password" placeholder="Password">
+	  	<input type="submit" value="Log In">
+	</form>
+</body>
+</html>
