44880abf3e62abf72de7ef92be1a0fac951c188b
Xiaken <jaybayron400@gmail.com>
2021-10-07 00:18:17 +0800

    Update emp_db.sql
    push

--- emp_db.sql
+++ emp_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2021 at 04:10 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Oct 06, 2021 at 06:09 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,52 +18,48 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `emp_db`
+-- Database: `management`
 --
+CREATE DATABASE IF NOT EXISTS `management` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `management`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `departments`
+-- Table structure for table `department`
 --
 
-CREATE TABLE `departments` (
-  `department_id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `date_time_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_time_updated` datetime NOT NULL DEFAULT current_timestamp(),
-  `remarks` varchar(30) NOT NULL
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL,
+  `department` varchar(20) DEFAULT NULL,
+  `date_time_created` datetime NOT NULL,
+  `date_time_updated` datetime NOT NULL,
+  `remarks` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `departments`
---
-
-INSERT INTO `departments` (`department_id`, `name`, `date_time_created`, `date_time_updated`, `remarks`) VALUES
-(1, 'Hr Department', '2021-09-30 09:18:31', '2021-09-30 09:18:31', ''),
-(2, 'It Department ', '2021-09-30 09:18:31', '2021-09-30 09:18:31', ''),
-(3, 'Finance Department', '2021-09-30 15:20:47', '2021-09-30 15:20:47', '');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employees`
+-- Table structure for table `employee`
 --
 
-CREATE TABLE `employees` (
-  `employee_id` int(11) NOT NULL,
-  `fullname` varchar(30) NOT NULL,
-  `address` varchar(30) NOT NULL,
-  `gender` char(8) NOT NULL,
-  `mobile` varchar(12) NOT NULL,
-  `city` varchar(30) NOT NULL,
-  `company` varchar(30) NOT NULL,
-  `position` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `department_id` int(11) NOT NULL,
-  `date_time_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_time_updated` datetime NOT NULL DEFAULT current_timestamp(),
-  `remarks` varchar(30) NOT NULL
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `Department_id` int(11) NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `m_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `b_date` date DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `c_number` int(11) DEFAULT NULL,
+  `h_number` int(11) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `date_time_created` datetime NOT NULL,
+  `date_time_updated` datetime NOT NULL,
+  `remarks` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -73,13 +69,18 @@ CREATE TABLE `employees` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `employee_id` int(11) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `date_time_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_time_updated` datetime NOT NULL DEFAULT current_timestamp(),
-  `remarks` varchar(30) NOT NULL
+  `id` int(11) NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `m_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `c_number` int(11) DEFAULT NULL,
+  `h_number` int(11) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `pass` varchar(20) DEFAULT NULL,
+  `date_time_created` datetime NOT NULL,
+  `date_time_updated` datetime NOT NULL,
+  `remarks` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -87,44 +88,44 @@ CREATE TABLE `users` (
 --
 
 --
--- Indexes for table `departments`
+-- Indexes for table `department`
 --
-ALTER TABLE `departments`
-  ADD PRIMARY KEY (`department_id`);
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `employees`
+-- Indexes for table `employee`
 --
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`employee_id`);
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `departments`
+-- AUTO_INCREMENT for table `department`
 --
-ALTER TABLE `departments`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `employees`
+-- AUTO_INCREMENT for table `employee`
 --
-ALTER TABLE `employees`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
