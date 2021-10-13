73c75cb8113c388566eef052dd127cd2796846c7
Xiaken <jaybayron400@gmail.com>
2021-10-13 22:38:18 +0800

    Update emp_db.sql
    push

--- db/emp_db.sql
+++ db/emp_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2021 at 12:59 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Oct 13, 2021 at 04:31 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `emp_db`
 --
+CREATE DATABASE IF NOT EXISTS `emp_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `emp_db`;
 
 -- --------------------------------------------------------
 
@@ -82,6 +84,27 @@ INSERT INTO `employees` (`employee_id`, `fullname`, `address`, `gender`, `mobile
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `date_time_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_time_updated` datetime NOT NULL DEFAULT current_timestamp(),
+  `remarks` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `employee_id`, `user_name`, `password`, `date_time_created`, `date_time_updated`, `remarks`) VALUES
+(3, 1, 'jay', '123', '2021-10-13 21:22:06', '2021-10-13 21:22:06', '');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -101,29 +124,17 @@ ALTER TABLE `employees`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `departments`
---
-ALTER TABLE `departments`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `employees`
---
-ALTER TABLE `employees`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
