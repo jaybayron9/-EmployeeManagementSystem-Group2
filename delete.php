e977f04df65182194ee88f8e71437e2a251b4e48
Jay Bayron <82939971+jaybayron9@users.noreply.github.com>
2021-10-15 12:03:24 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,6 +6,6 @@ $employee_id = $_GET['employee_id'];
 $sql = " DELETE FROM employees WHERE employee_id = $employee_id " ;
 $query = mysqli_query($con,$sql);
 
-header("location:index.php");
+header("location:home.php");
 
 ?>
